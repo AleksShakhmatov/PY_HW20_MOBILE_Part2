783256b6901b41c70e650115dc85b8ca03354d56
AleksSh <Coluchy@yandex.ru>
2025-02-12 02:00:22 +0600

    c

--- tests/wiki_test.py
+++ tests/wiki_test.py
@@ -4,53 +4,56 @@ from appium.webdriver.common.appiumby import AppiumBy
 from selene import browser, have
 
 
-@allure.story("Запускаем приложение Википедии в первый раз")
-@allure.severity(allure.severity_level.CRITICAL)
-def test_getting_started():
-    with allure.step('Проверяем первую страницу'):
+@allure.story('Search')
+@allure.severity(allure.severity_level.NORMAL)
+@allure.tag('Mobile')
+def test_skip_pages():
+    with allure.step('Проверка первой страницы'):
         browser.element((AppiumBy.ID, 'org.wikipedia.alpha:id/primaryTextView')).should(
             have.text('The Free Encyclopedia\n…in over 300 languages'))
-        browser.element((AppiumBy.ID, 'org.wikipedia.alpha:id/secondaryTextView')).should(
-            have.text('We’ve found the following on your device:'))
-    with allure.step('Проверяем вторую страницу'):
+    with allure.step('Проверка второй страницы'):
         browser.element((AppiumBy.ID, 'org.wikipedia.alpha:id/fragment_onboarding_forward_button')).click()
         browser.element((AppiumBy.ID, 'org.wikipedia.alpha:id/primaryTextView')).should(
             have.text('New ways to explore'))
-    with allure.step('Проверяем третью страницу'):
+    with allure.step('Проверка третьей страницы'):
         browser.element((AppiumBy.ID, 'org.wikipedia.alpha:id/fragment_onboarding_forward_button')).click()
         browser.element((AppiumBy.ID, 'org.wikipedia.alpha:id/primaryTextView')).should(
             have.text('Reading lists with sync'))
-    with allure.step('Проверяем четвёртую страницу'):
+    with allure.step('Проверка четвёртой страницы'):
         browser.element((AppiumBy.ID, 'org.wikipedia.alpha:id/fragment_onboarding_forward_button')).click()
         browser.element((AppiumBy.ID, 'org.wikipedia.alpha:id/primaryTextView')).should(have.text('Data & Privacy'))
 
 
-@allure.story("Ищем в википедии Appium")
-def test_search():
-    with step('Вводим "Appium" в поиске Википедии'):
-        browser.element((AppiumBy.ID, 'org.wikipedia.alpha:id/fragment_onboarding_skip_button')).click()
+@allure.story('Search')
+@allure.severity(allure.severity_level.NORMAL)
+@allure.tag('Mobile')
+@allure.feature('Поиск в википедии текста "Appium"')
+def test_search_wiki_appium():
+    with step('Ввести "Appium" в поиске Википедии'):
+        browser.element((AppiumBy.ID, 'org.wikipedia.alpha:id/fragment_onboarding_forward_button')).click()
         browser.element((AppiumBy.ACCESSIBILITY_ID, "Search Wikipedia")).click()
         browser.element((AppiumBy.ID, "org.wikipedia.alpha:id/search_src_text")).type('Appium')
 
-    with step('Проверяем наименование результата'):
+    with step('Проверить результат поиска'):
         results = browser.all((AppiumBy.ID, 'org.wikipedia.alpha:id/page_list_item_title'))
         results.should(have.size_greater_than(0))
         results.first.should(have.text('Appium'))
 
 
-@allure.story("Ищем в википедии Devil May Cry и кликаем его")
-@allure.severity(allure.severity_level.CRITICAL)
-def test_open_first_article_DMC():
-    with step('Вводим "Devil May Cry" в поиске Википедии'):
-        browser.element((AppiumBy.ID, 'org.wikipedia.alpha:id/fragment_onboarding_skip_button')).click()
+@allure.story('Search')
+@allure.severity(allure.severity_level.NORMAL)
+@allure.tag('Mobile')
+@allure.feature('Поиск в википедии текста "Moto"')
+def test_search_wiki_moto():
+    with step('Ввести "Moto" в поиске Википедии'):
+        browser.element((AppiumBy.ID, 'org.wikipedia.alpha:id/fragment_onboarding_forward_button')).click()
         browser.element((AppiumBy.ACCESSIBILITY_ID, "Search Wikipedia")).click()
-        browser.element((AppiumBy.ID, "org.wikipedia.alpha:id/search_src_text")).type('Devil May Cry')
+        browser.element((AppiumBy.ID, "org.wikipedia.alpha:id/search_src_text")).type('Moto')
 
-    with step('Проверяем наименование результата'):
+    with step('Проверить результат поиска'):
         results = browser.all((AppiumBy.ID, 'org.wikipedia.alpha:id/page_list_item_title'))
         results.should(have.size_greater_than(0))
-        results.first.should(have.text('Devil May Cry'))
+        results.first.should(have.text('Moto'))
 
-    with step('Кликаем элемент'):
+    with step('Кликнуть на элемент'):
         browser.all((AppiumBy.ID, 'org.wikipedia.alpha:id/page_list_item_title')).first.click()
-
